7853ef133d2a76af51107facc86941c4392ff614
ayari <asma.ayari@esprit.tn>
2015-02-18 14:26:46 +0100

    add testimonials

---
+++ wp-content/themes/linaya/temoignages.php
@@ -0,0 +1,80 @@
+   <div class=" col-lg-3 col-lg-offset-1 ">
+                <h4 class="color-titre-menu">Temoignages
+                </h4>
+
+
+                <div id="wrapper">
+
+
+                        <div class="testimonials-slider">
+
+                            <div class="slide">
+                                <div class="testimonials-carousel-context">
+
+                                    <div class="testimonials-carousel-content"><p class="color-content" >Sed posuere consectetur est at lobortis. Fusce dapibus, tellus ac cursus commodo.Cras mattis consectetur purus sit amet fermentum. /p></div>
+                                    <div class="testimonials-name">Bill Robinson <span>google.com</span></div>
+
+                                </div>
+
+                            </div>
+
+                            <div class="slide">
+                                <div class="testimonials-carousel-context">
+                                    <div class="testimonials-carousel-content"><p class="color-content">Cras mattis consectetur purus sit amet fermentum.</p></div>
+                                    <div class="testimonials-name">Michael Rocks <span>yahoo.com</span></div>
+
+                                </div>
+                            </div>
+
+                            <div class="slide">
+                                <div class="testimonials-carousel-context">
+                                    <div class="testimonials-carousel-content"><p class="color-content"> The point of using Lorem Ipsum is that it has a more-or-less normal..</p></div>
+                                    <div class="testimonials-name">Andrew Bilson <span>dailydesignnotes.com</span></div>
+
+                                </div>
+                            </div>
+
+                        </div>
+
+                    <a href="#" class="style-titre"> Voir Tous Les Témoignages <img src="image/flèche.PNG"></a>
+
+                    </div>
+
+
+                <!-- DEMO JS -->
+                <script>
+                    $('.testimonials-slider').bxSlider({
+                        slideWidth: 800,
+                        minSlides: 2,
+                        maxSlides: 2,
+                        slideMargin: 32,
+                        auto: true,
+                        autoControls: true
+                    });
+                </script>
+                <!-- END DEMO JS -->
+
+
+                <!-- DONT COPY THESE LINES (These codes our demo codes. Please don't copy) -->
+                <!-- WDT Analytics -->
+                <script>
+                    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+                    ga('create', 'UA-40730469-1', 'webdesigntunes.com');
+                    ga('send', 'pageview');
+
+                </script>
+
+                <div style="display:none;">
+                    <script id="_wau49x">var _wau = _wau || []; _wau.push(["small", "l2ejtaf1o4ra", "49x"]);
+                    (function() {var s=document.createElement("script"); s.async=true;
+                        s.src="http://widgets.amung.us/small.js";
+                        document.getElementsByTagName("head")[0].appendChild(s);
+                    })();</script>
+                </div>
+
+
+            </div>
